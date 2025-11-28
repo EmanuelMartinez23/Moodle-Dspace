abdd39188aa59816b2cf764328445640e34fc64f
emanuelmartinezfelipe <developeremanuel13@gmail.com>
2025-11-28 15:03:36 -0600

    Ajuste de error de cache

--- blocks/dspace_integration/block_dspace_integration.php
+++ blocks/dspace_integration/block_dspace_integration.php
@@ -71,23 +71,8 @@ class block_dspace_integration extends block_base {
                         _dtLoadingStarted = true;
                         injectOnce('link', {id: 'dt-bs5-css', rel: 'stylesheet', href: CDN.css});
 
-                        var savedDefine = window.define;
-                        var savedModule = window.module;
-                        var hadDefine = Object.prototype.hasOwnProperty.call(window, 'define');
-                        var hadModule = Object.prototype.hasOwnProperty.call(window, 'module');
-
-                        function suppressAMD(){
-                            try {
-                                window.define = undefined;
-                                window.module = undefined;
-                            } catch(e){}
-                        }
-                        function restoreAMD(){
-                            try {
-                                if (hadDefine) { window.define = savedDefine; } else { delete window.define; }
-                                if (hadModule) { window.module = savedModule; } else { delete window.module; }
-                            } catch(e){}
-                        }
+                        // IMPORTANTE: No manipular RequireJS (define/module) del core de Moodle.
+                        // Interferir con AMD puede romper la carga del editor (p.ej., "No define call for jquery").
 
                         function loadScript(id, src, cb){
                             if (document.getElementById(id)) {
@@ -107,10 +92,9 @@ class block_dspace_integration extends block_base {
                             (document.head || document.documentElement).appendChild(s);
                         }
 
-                        suppressAMD();
+                        // Cargamos DataTables JS respetando el entorno AMD de la página.
                         loadScript('dt-core-js', CDN.jsjq, function(){
                             loadScript('dt-bs5-js', CDN.jsbs, function(){
-                                restoreAMD();
                                 if (callback) callback();
                             });
                         });
