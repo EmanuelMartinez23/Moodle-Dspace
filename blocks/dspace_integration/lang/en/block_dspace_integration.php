a822a013524df315edd2364317abe5a63bc83d65
emanuelmartinezfelipe <developeremanuel13@gmail.com>
2025-12-03 05:51:22 -0600

    Se agrega strings faltantes del plugin

--- blocks/dspace_integration/lang/en/block_dspace_integration.php
+++ blocks/dspace_integration/lang/en/block_dspace_integration.php
@@ -1,7 +1,13 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-// Language strings for block_dspace_integration (English)
+$string['pluginname'] = 'Dspace: Obtener comunidades';
+$string['block_dspace_integration:addinstance'] = 'Añadir bloque de Dspace';
+$string['block_dspace_integration:myaddinstance'] = 'Añadir bloque de Dspace a la página Mi Moodle';
 
-defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'DSpace integration';
+$string['server'] = 'DSpace Server URL';
+$string['server_desc'] = 'URL del servidor DSpace para la API';
+$string['email'] = 'API User Email';
+$string['email_desc'] = 'Email de la cuenta para acceder a la API';
+$string['password'] = 'API Password';
+$string['password_desc'] = 'Contraseña de la cuenta de API';
+
